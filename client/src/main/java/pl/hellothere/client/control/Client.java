429b3a7d4fbdafde570263cff5fb2a89bc883bb6
julianles <julian.lesniak@student.uj.edu.pl>
2021-04-18 13:26:30 +0200

    Zmiana Client na opartego na Application

--- client/src/main/java/pl/hellothere/client/control/Client.java
+++ client/src/main/java/pl/hellothere/client/control/Client.java
@@ -1,31 +1,27 @@
 package pl.hellothere.client.control;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
 import pl.hellothere.client.network.ServerClient;
 import pl.hellothere.client.view.controller.ClientViewController;
 
-public class Client {
-    final ServerClient connection;
-
-    public Client() {
+public class Client extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        final ServerClient connection;
         try {
             connection = new ServerClient();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            ClientViewController.showErrorMessage("No connection");
+            return;
         }
-    }
 
-    public void run() {
-        ClientViewController.setCredentialsTest(connection::signIn);
-        Application.launch(ClientViewController.class);
+        ClientViewController.getLoginView().setCredentialsTest(connection::signIn);
+        ClientViewController.getLoginView().setSignInAction((a, b) -> System.out.println("Login: "+a+" Password: "+b));
+        ClientViewController.getLoginView().run();
     }
 
     public static void main(String[] args) {
-        try {
-            Client c = new Client();
-            c.run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        launch(args);
     }
 }
--- client/src/main/java/pl/hellothere/client/view/controller/ClientViewController.java
+++ client/src/main/java/pl/hellothere/client/view/controller/ClientViewController.java
@@ -1,6 +1,10 @@
 package pl.hellothere.client.view.controller;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import pl.hellothere.client.view.login.ClientViewLogin;
 
@@ -11,16 +15,26 @@ public class ClientViewController extends Application {
     static BiFunction<String, String, Boolean> credentialsTest;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        if(credentialsTest == null) throw new NoCredentialsTestException();
-        cvl0.setCredentialsTest(credentialsTest);
-        cvl0.start(primaryStage);
-    }
+    public void start(Stage primaryStage) { }
 
-    static public void setCredentialsTest(BiFunction<String, String, Boolean> credentialsTest) {
-        ClientViewController.credentialsTest = credentialsTest;
+    static public ClientViewLogin getLoginView() { return cvl0; }
+
+    static public void close() { Platform.exit(); }
+
+    static public void showErrorMessage(String errorMessage) {
+        Stage errorStage = new Stage();
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: AliceBlue;");
+        Label errorLabel = new Label(errorMessage);
+        errorLabel.setStyle("-fx-font-family: Lucida Console; -fx-text-fill: Red; -fx-font-size: 20; -fx-wrap-text: true");
+        root.getChildren().add(errorLabel);
+
+        errorStage.setTitle("Hello There Error");
+        errorStage.setScene(new Scene(root, 300, 100));
+        errorStage.setResizable(false);
+
+        errorStage.show();
     }
 
     public static class NoCredentialsTestException extends Exception {}
 }
-
