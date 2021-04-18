84d1a01c0e7aec505e94fceb4b9c742763ad368c
Hubert Zięba <hub.zieba@student.uj.edu.pl>
2021-04-18 18:25:25 +0200

    Ekran logowania

--- client/src/main/java/pl/hellothere/client/control/Client.java
+++ client/src/main/java/pl/hellothere/client/control/Client.java
@@ -1,14 +1,51 @@
 package pl.hellothere.client.control;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import pl.hellothere.client.network.ServerClient;
 import pl.hellothere.client.view.controller.ClientViewController;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Client extends Application {
+    static Client client;
+
+    ServerClient connection;
+    ExecutorService ex = Executors.newCachedThreadPool();
+
+    AtomicBoolean logging = new AtomicBoolean(false);
+
+    void signIn(String login, String password) {
+        ex.submit(() -> {
+            if(!logging.compareAndSet(false, true))
+                return;
+
+            try {
+                if(connection.signIn(login, password))
+                    Platform.runLater(() -> {
+                        ClientViewController.getLoginView().close();
+                        startMainApp();
+                    });
+                else
+                    Platform.runLater(ClientViewController.getLoginView()::loginFailMessage);
+            } catch (Exception e) {
+                Platform.runLater(() -> {
+                    ClientViewController.showErrorMessage("No connection");
+                    ClientViewController.getLoginView().close();
+                });
+            } finally {
+                logging.set(false);
+            }
+        });
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-        final ServerClient connection;
+        client = this;
+
         try {
             connection = new ServerClient();
         } catch (Exception e) {
@@ -16,11 +53,20 @@ public class Client extends Application {
             return;
         }
 
-        ClientViewController.getLoginView().setSignInAction((a, b) -> System.out.println("Submitted"));
+        ClientViewController.getLoginView().setSignInAction(this::signIn);
         ClientViewController.getLoginView().run();
+    }
+
+    void startMainApp() {
+        ClientViewController.showErrorMessage("Welcome!");
+    }
+
+    public void close() {
+        ex.shutdown();
     }
 
     public static void main(String[] args) {
         launch(args);
+        client.close();
     }
 }
