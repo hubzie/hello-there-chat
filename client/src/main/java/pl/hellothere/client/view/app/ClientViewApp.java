ca5e0208894dd6ba646d7ba8606f85518c3bf88e
julianles <julian.lesniak@student.uj.edu.pl>
2021-06-13 22:31:20 +0200

    Poprawki wyglądu

--- client/src/main/java/pl/hellothere/client/view/app/ClientViewApp.java
+++ client/src/main/java/pl/hellothere/client/view/app/ClientViewApp.java
@@ -112,7 +112,7 @@ public class ClientViewApp extends Application {
             setScrollMessagesToBottom();
 
             cvac.groupsBox.getChildren().removeIf(e -> (e instanceof GroupMember));
-            if(curGroup.getName() != null || conversationDetails.getUsers().size() > 2) {
+            if(conversationDetails.getUsers().size() > 2) {
                 int ind = cvac.groupsBox.getChildren().indexOf(convButtonMap.get(curGroup));
                 for(UserData i : conversationDetails.getUsers()) if(i.getID() != curUserID) cvac.groupsBox.getChildren().add(++ind, new GroupMember(i));
             }
